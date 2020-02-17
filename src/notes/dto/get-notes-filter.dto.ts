1f3be6b2db1806aac9834e0b57b4cdd18f42f3d7
harut <har17bar@gmail.com>
2020-02-17 17:51:18 +0400

    delete unused code

--- src/notes/dto/get-notes-filter.dto.ts
+++ src/notes/dto/get-notes-filter.dto.ts
@@ -1,7 +1,5 @@
-import { IsIn, IsNotEmpty, IsOptional } from 'class-validator';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
-
-// const allowedStatus: TaskStatus[] = Object.values(TaskStatus);
 
 export class GetNotesFilterDto {
   @ApiProperty({ required: false })
